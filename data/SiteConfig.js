ab70f019db7863e24967141dbcd95d91d9b29e91
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 11:45:26 +0200

    Update site config.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,17 +2,17 @@ module.exports = {
   siteTitle: "Gatsby WordPress Starter", // Site title.
   siteTitleAlt: "GatsbyJS WordPress API Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://ericwindmill.com", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-wordpress-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteUrl: "https://", // Domain of your website without pathPrefix.
+  // pathPrefix: "/gatsby-wordpress-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A GatsbyJS stater with the WordPress API built  in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
-  disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleAnalyticsID: "", // GA tracking ID.
+  disqusShortname: "", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
-  userName: "User", // Username to display in the author segment.
-  userTwitter: "@ericwindmill", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "Portland, Oregon, USA ", // User location to display in the author segment.
+  userName: "gc", // Username to display in the author segment.
+  userTwitter: "@GatsbyCentral", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Berlin", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
@@ -20,18 +20,13 @@ module.exports = {
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-advanced-starter",
+      url: "https://github.com/GatsbyCentral/",
       iconClassName: "fa fa-github"
     },
     {
       label: "Twitter",
-      url: "https://twitter.com/Vagr9K",
+      url: "https://twitter.com/GatsbyCentral",
       iconClassName: "fa fa-twitter"
-    },
-    {
-      label: "Email",
-      url: "mailto:vagr9k@gmail.com",
-      iconClassName: "fa fa-envelope"
     }
   ],
   copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
