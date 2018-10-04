3db298694093678b086a8fde16a9a9237eaea049
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 18:04:17 +0200

    WIP. Dynamically generate menu from pages.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,55 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
+  <StaticQuery
+    query={graphql`
+      query {
+        allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
+          edges {
+            node {
+              title
+              slug
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <nav className="navbar is-transparent">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure className="image">
+                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              </figure>
+            </Link>
+          </div>
+          <div className="navbar-start">
+            {data.allWordpressPage.edges.map(edge => (
+              <Link className="navbar-item" to={edge.node.slug}>
+                {edge.node.title}
+              </Link>
+            ))}
+          </div>
+          <div className="navbar-end">
+            <a
+              className="navbar-item"
+              href="https://github.com/GatsbyCentral/gatsby-starter-wordpress"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon">
+                <img src={github} alt="Github" />
+              </span>
+            </a>
+          </div>
+        </div>
+      </nav>
+    )}
+  />
 )
 
 export default Navbar
