4538edf6ea397ca27e75015468779b0a1f687adc
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 12:43:55 +0200

    Hide the "Continue reading" links. #17
    This is a workaround / hack. It's just a CSS `display: none;`. We should probably do something better.

--- src/components/Posts/PostListing/PostListing.jsx
+++ src/components/Posts/PostListing/PostListing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "gatsby-link";
+import "./PostListing.css";
 
 class PostListing extends React.Component {
   getPostList() {
