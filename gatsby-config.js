d945ffafbd4e55cfb87ab147041bc30405d41311
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 16:25:44 +0200

    Remove netlify-cms.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,12 +27,6 @@ module.exports = {
         plugins: [],
       },
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
