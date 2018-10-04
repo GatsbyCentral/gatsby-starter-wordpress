c89a88ba0822b28eaf6879bea1f381f681f15acf
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 14:27:45 +0200

    Add a 900px wrapper around WP pages.

--- src/templates/wp_page.jsx
+++ src/templates/wp_page.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import TopNavigation from "../components/Layout/Navigation/Navigation";
 
 class wpPage extends Component {
@@ -13,16 +14,32 @@ class wpPage extends Component {
     return (
       <div>
         <TopNavigation pages={this.props.data.allWordpressPage} />
-        <h1>{pageNode.title}</h1>
-        <main>
-          <div dangerouslySetInnerHTML={{ __html: pageNode.content }} />
-        </main>
+        <Container>
+          <h1>{pageNode.title}</h1>
+          <main>
+            <div dangerouslySetInnerHTML={{ __html: pageNode.content }} />
+          </main>
+        </Container>
       </div>
     );
   }
 }
 
 export default wpPage;
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 100px auto;
+
+  .tags {
+    display: flex;
+    flex-flow: row;
+    margin-top: 50px;
+    h4 {
+      width: 200px;
+    }
+  }
+`;
 
 /* eslint no-undef: "off"*/
 export const pageQuery = graphql`
