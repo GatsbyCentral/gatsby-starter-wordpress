02112e51abcd491963a8b145ccce634f9db74645
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 18:13:40 +0200

    WIP. Remove unused imports. Cleanup.

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
 
 export const PageTemplate = ({ title, content }) => {
   return (
