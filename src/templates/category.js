0473e4d3b2de4ea7efb795f65f7e31ce05a1b6c2
Callum Macdonald <git@callum-macdonald.com>
2018-10-04 17:56:26 +0200

    WIP. Working category page.

--- src/templates/category.js
+++ src/templates/category.js
@@ -5,20 +5,20 @@ import Layout from '../components/Layout'
 
 class TagRoute extends React.Component {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges
+    const posts = this.props.data.allWordpressPost.edges
     const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+      <li key={post.node.slug}>
+        <Link to={post.node.slug}>
+          <h2 className="is-size-2">{post.node.title}</h2>
         </Link>
       </li>
     ))
-    const tag = this.props.pageContext.tag
+    const tag = this.props.pageContext.name
     const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
+    const totalCount = this.props.data.allWordpressPost.totalCount
     const tagHeader = `${totalCount} post${
       totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`
+    } in the “${tag}” category`
 
     return (
       <Layout>
