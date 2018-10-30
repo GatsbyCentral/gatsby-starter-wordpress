44159340c290d51eb810fcda792f072a0dbccf55
Callum Macdonald <git@callum-macdonald.com>
2018-10-30 13:22:38 +0100

    Minor linting fix.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import Navbar from '../components/Navbar'
+import Navbar from './Navbar'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
