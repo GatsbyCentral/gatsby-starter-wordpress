f156cdc2ddc9a8394a6f90917f267a6675bed442
Callum Macdonald <git@callum-macdonald.com>
2018-10-30 13:59:49 +0100

    Use `PostList` for tags, categories, and index.
    Also includes some minor refactoring.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,42 +4,48 @@ import { Link, graphql } from 'gatsby'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { posts } = this.props
+    const { posts, title } = this.props
 
     return (
-      <div>
-        {posts.map(({ node: post }) => (
-          <div
-            className="content"
-            style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-            key={post.id}
-          >
-            <p>
-              <Link className="has-text-primary" to={post.slug}>
-                {post.title}
-              </Link>
-              <span> &bull; </span>
-              <small>{post.date}</small>
-            </p>
-            <div>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: post.excerpt.replace(/<p class="link-more.*/, ''),
-                }}
-              />
-              <Link className="button is-small" to={post.slug}>
-                Keep Reading →
-              </Link>
-            </div>
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">{title}</h1>
           </div>
-        ))}
-      </div>
+          {posts.map(({ node: post }) => (
+            <div
+              className="content"
+              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+              key={post.id}
+            >
+              <p>
+                <Link className="has-text-primary" to={post.slug}>
+                  {post.title}
+                </Link>
+                <span> &bull; </span>
+                <small>{post.date}</small>
+              </p>
+              <div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: post.excerpt.replace(/<p class="link-more.*/, ''),
+                  }}
+                />
+                <Link className="button is-small" to={post.slug}>
+                  Keep Reading →
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     )
   }
 }
 
 IndexPage.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
 }
 
 export const pageQuery = graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,7 @@ export default class IndexPage extends React.Component {
 
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-            </div>
-            <PostList posts={posts} />
-          </div>
-        </section>
+        <PostList posts={posts} title="Latest posts" />
       </Layout>
     )
   }
