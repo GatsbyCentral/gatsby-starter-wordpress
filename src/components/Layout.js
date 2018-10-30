7403ad536c60fbfdf9d696779eba4e01526fe14f
Callum Macdonald <git@callum-macdonald.com>
2018-10-30 13:22:05 +0100

    Tweak the default title.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Gatsby + WordPress" />
     <Navbar />
     <div>{children}</div>
   </div>
