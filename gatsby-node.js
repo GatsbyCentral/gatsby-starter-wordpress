8c005d99565fb5cf243b1821a721736262305968
Callum Macdonald <git@callum-macdonald.com>
2018-11-05 14:29:18 +0100

    Get WP base URL from config.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,27 @@ const _ = require('lodash')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
+const config = require('./gatsby-config.js')
+
+// NOTE: In Gatsby Commit 2235bf9 normalisation of the base URL was added. This
+// change adds support for base URLs including a protocol. In our processing of
+// the base URL here, this is not supported. We might need to address this at
+// some point.
+// https://github.com/gatsbyjs/gatsby/commit/2235bf991d908c562fc7a6d2dccefc842599bc76
+
+// Get the WordPress base URL from the config file
+const wpConfigOptions = _.chain(config)
+  .get('plugins')
+  .find({
+    resolve: 'gatsby-source-wordpress',
+  })
+  .get('options')
+  .value()
+const wpBaseUrl = `${_.get('baseUrl', wpConfigOptions)}://${_.get(
+  'baseUrl',
+  wpConfigOptions
+)}`
+
 /**
  * Create nested paths
  * e.g., /grandparent/parent/node-slug
@@ -14,10 +35,9 @@ exports.sourceNodes = ({ getNodes, actions }) => {
   const pageNodes = getNodes().filter(
     node => node.internal.type === 'wordpress__PAGE'
   )
-  const sourceURL = `https://wpdemo.gatsbycentral.com`
   // Build each node's path
   pageNodes.forEach(node => {
-    const nestedSlug = `/${node.link.replace(sourceURL, '')}/`
+    const nestedSlug = `/${node.link.replace(wpBaseUrl, '')}/`
     // Add full path to node -- available at node.fields.path
     createNodeField({
       node,
