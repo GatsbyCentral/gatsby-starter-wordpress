741ecc841182a949720b5af5642ecfd48bb6daca
David Carter <dajocarter@gmail.com>
2018-11-05 17:27:50 -0800

    [WIP] Add linked numbers to pagination
    WIP => Need to fix pathPrefix. Not exposed in pageContext
    
    Copies pagination syntax from WP.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Pagination = ({ pageContext }) => {
-  const { previousPagePath, nextPagePath } = pageContext
+const Pagination = ({ pageContext, endSize = 1, midSize = 1 }) => {
+  const {
+    previousPagePath,
+    nextPagePath,
+    numberOfPages,
+    humanPageNumber,
+  } = pageContext
+  let dots = false
 
   return (
     <nav className="pagination" role="navigation">
@@ -14,6 +20,40 @@ const Pagination = ({ pageContext }) => {
             </Link>
           </div>
         )}
+        {Array.from({ length: numberOfPages }, (_, i) => {
+          const index = i + 1
+          const renderLink =
+            index <= endSize ||
+            (index >= humanPageNumber - midSize &&
+              index <= humanPageNumber + midSize) ||
+            index > numberOfPages - endSize
+          let NavItem = null
+          if (index === humanPageNumber) {
+            dots = true
+            NavItem = (
+              <div className="navbar-item" key={`page-${index}`}>
+                <span aria-current="page" className="current">
+                  {index}
+                </span>
+              </div>
+            )
+          } else if (renderLink) {
+            dots = true
+            NavItem = (
+              <div className="navbar-item" key={`page-${index}`}>
+                <Link to={i === 0 ? `/` : `/${index}`}>{index}</Link>
+              </div>
+            )
+          } else if (dots) {
+            dots = false
+            NavItem = (
+              <div className="navbar-item" key={`page-${index}`}>
+                <span className="dots">&hellip;</span>
+              </div>
+            )
+          }
+          return NavItem
+        })}
         {nextPagePath && (
           <div className="navbar-item">
             <Link to={nextPagePath} rel="next">
