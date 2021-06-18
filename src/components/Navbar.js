26b83e1c0fd1e37f9e19339a2f4d346279646ebe
Ben <b.patrickr@gmail.com>
2021-06-18 12:48:31 -0700

    Add responsive hamburger menu button to navbar with state hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
-const Navbar = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
-          edges {
-            node {
-              title
-              slug
+const Navbar = () => {
+
+  const [burgerIsActive, setBurgerIsActive] = useState(false)
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
+            edges {
+              node {
+                title
+                slug
+              }
             }
           }
         }
-      }
-    `}
-    render={data => (
-      <nav className="navbar is-transparent">
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-              <figure className="image">
-                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-              </figure>
-            </Link>
-          </div>
-          <div className="navbar-start">
-            {data.allWordpressPage.edges.map(edge => (
-              <Link
-                className="navbar-item"
-                to={edge.node.slug}
-                key={edge.node.slug}
-              >
-                {edge.node.title}
+      `}
+      render={data => (
+        <nav className="navbar is-transparent">
+          <div className="container">
+            <div className="navbar-brand">
+              <Link to="/" className="navbar-item">
+                <figure className="image">
+                  <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+                </figure>
               </Link>
-            ))}
+              <a
+                onClick={() => {
+                  setBurgerIsActive(!burgerIsActive)
+                }}
+                role="button"
+                className={`navbar-burger burger ${
+                  burgerIsActive ? 'is-active' : ''
+                }`}
+                aria-label="menu"
+                aria-expanded="false"
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+            <div className={`navbar-menu ${burgerIsActive ? 'is-active' : ''}`}>
+              <div className="navbar-start">
+                {data.allWordpressPage.edges.map(edge => (
+                  <Link
+                    className="navbar-item"
+                    to={edge.node.slug}
+                    key={edge.node.slug}
+                  >
+                    {edge.node.title}
+                  </Link>
+                ))}
+              </div>
+              <div className="navbar-end">
+                <a
+                  className="navbar-item"
+                  href="https://github.com/GatsbyCentral/gatsby-starter-wordpress"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="icon">
+                    <img src={github} alt="Github" />
+                  </span>
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="navbar-end">
-            <a
-              className="navbar-item"
-              href="https://github.com/GatsbyCentral/gatsby-starter-wordpress"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
-          </div>
-        </div>
-      </nav>
-    )}
-  />
-)
+        </nav>
+      )}
+    />
+  )
+}
 
 export default Navbar
